1df2bc1fde354ddb8d4806090cc6ea96337b252a
capn <jamesgreen.311@gmail.com>
2020-12-23 13:50:49 -0500

    Updated web app url

--- Service.js
+++ Service.js
@@ -11,13 +11,14 @@ function getRangeByName(n) {
 }
 
 function sendNotification(data) {
-    const regForm = "https://script.google.com/macros/s/AKfycbzDUr5Ajpo3MmAP1WQstYazIwp4BsIX9UjAembFd18/exec?v=reg&id=73CB67";
+    const regForm = "https://script.google.com/macros/s/AKfycbw9yov3sDA-4K0N4K5bdj_78HyM6idKyGUmz_Cv9oIHPpGKGzc/exec?v=reg&id=73CB67";
     const eventsPage = "https://metrorichmondart.org/meetings-critiques/";
     const cal = ics();
     cal.addEvent(data.title, data.subtitle, data.location, data.start, data.end);
     const attach = cal.build();
+    const contactInfo = data.contactEmail?`For questions, please contact ${data.contactEmail} <br><br>`:"";
     const body = `Thank you for registering for the ${data.title} <br><br>
-        For questions please contact ${data.contactEmail} <br><br>
+        ${contactInfo}
         If you need a reminder about event details, please revisit the <a href='${regForm}'>registration form</a> or the MRAA website 
         <a href='${eventsPage}'>Events page</a>`;
     const blob = Utilities.newBlob(attach, "text/calendar", "event.ics");
